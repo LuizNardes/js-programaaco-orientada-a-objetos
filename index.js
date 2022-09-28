142f0dea9b3cf573160a80c633400e35a9f7c1b9
Luiz Nardes <89425839+LuizNardes@users.noreply.github.com>
2022-09-28 20:08:15 -0300

    aula-5-conclusao

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ console.log(novoUser.exibirInfos());
  const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
  console.log(novoAdmin.nome);
 
- novoAdmin.nome = ''
- console.log(novoAdmin.nome);
+/*  novoAdmin.nome = ''
+ console.log(novoAdmin.nome); */
+
+ const novoDocente = new Docente('Guilherme', 'g@g.com', '2021-01-01')
+ console.log(novoDocente.exibirInfos());
